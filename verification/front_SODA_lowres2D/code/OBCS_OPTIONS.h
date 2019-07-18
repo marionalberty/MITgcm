536f7a9b8fbd200b94b55bf4d4d4b263b6437b5a
Marion Alberty <malberty@princeton.edu>
2019-07-18 14:33:51 -0400

    Added east and west open boundary

--- verification/front_SODA_lowres2D/code/OBCS_OPTIONS.h
+++ verification/front_SODA_lowres2D/code/OBCS_OPTIONS.h
@@ -12,8 +12,8 @@ C Package-specific Options & Macros go here
 C Enable individual open boundaries
 #undef ALLOW_OBCS_NORTH
 #undef ALLOW_OBCS_SOUTH
-#undef ALLOW_OBCS_EAST
-#undef ALLOW_OBCS_WEST
+#define ALLOW_OBCS_EAST
+#define ALLOW_OBCS_WEST
 
 C This include hooks to the Orlanski Open Boundary Radiation code
 #undef ALLOW_ORLANSKI
