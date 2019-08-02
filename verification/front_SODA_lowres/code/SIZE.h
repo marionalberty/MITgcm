c8d4dcbfa7c24f639956f1a47abb1ae58328ce8c
Marion Alberty <malberty@princeton.edu>
2019-08-02 14:57:54 -0400

    Copied working 2D files to 3D case

--- verification/front_SODA_lowres/code/SIZE.h
+++ verification/front_SODA_lowres/code/SIZE.h
@@ -42,14 +42,14 @@ CEOP
       INTEGER Ny
       INTEGER Nr
       PARAMETER (
-     &           sNx =  25,
+     &           sNx =   1,
      &           sNy =  25,
      &           OLx =   3,
      &           OLy =   3,
      &           nSx =   1,
      &           nSy =   1,
-     &           nPx =   2,
-     &           nPy =   2,
+     &           nPx =   1,
+     &           nPy =   3,
      &           Nx  = sNx*nSx*nPx,
      &           Ny  = sNy*nSy*nPy,
      &           Nr  =  25)
